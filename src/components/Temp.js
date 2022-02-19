36548a13f093db040d5f37fdd03f52f5f83bcf36
tomer <tomerzt1@gmail.com>
2022-02-19 20:03:20 +0200

    cross domain test

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -7,6 +7,7 @@ export const getData = new function () {
 	this.data = async () => {
 	return	 await axios.get(
 			"http://blynk-cloud.com/NFmzesAKsa340A_zSVk5NUuKkALH99RD/get/V5",
+			{ crossdomain: true }
 		);
 		//return res.data[0];
 	};
